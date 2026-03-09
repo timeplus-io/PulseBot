a584b1affbd938c68db4f4ec3be5358632505768
Gang Tao <gang@timeplus.io>
2026-03-09 12:49:46 -0700

    remove version

--- pulsebot/_version.py
+++ pulsebot/_version.py
@@ -28,7 +28,7 @@ version_tuple: VERSION_TUPLE
 commit_id: COMMIT_ID
 __commit_id__: COMMIT_ID
 
-__version__ = version = '0.4.1.dev28+g7920857d9.d20260309'
-__version_tuple__ = version_tuple = (0, 4, 1, 'dev28', 'g7920857d9.d20260309')
+__version__ = version = '0.5.0'
+__version_tuple__ = version_tuple = (0, 5, 0)
 
 __commit_id__ = commit_id = None
